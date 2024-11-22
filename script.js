e7333ab31e28c9403f7cb4afcd15d6118ae43e09
Fellipe Castro <fellipecastro27d@gmail.com>
2024-11-22 14:44:20 -0300

    Projeto atualizado

--- script.js
+++ script.js
@@ -1,110 +1,115 @@
-const cardsContainer = document.getElementById('cards-container')
-const finalPointsMsg = document.getElementById('final-points-msg')
-const remaningPlays = document.getElementById('remaning-plays')
-const points = document.getElementById('points')
-const flashMsg = document.getElementById('flash-msg')
-const reloadBtn = document.getElementById('reload-btn')
+const cardsContainer = document.getElementById("cards-container");
+const finalPointsMsg = document.getElementById("final-points-msg");
+const remaningPlays = document.getElementById("remaning-plays");
+const points = document.getElementById("points");
+const flashMsg = document.getElementById("flash-msg");
+const reloadBtn = document.getElementById("reload-btn");
 
-let plays = 12
-let currentPoints = 0
+let plays = 12;
+let currentPoints = 0;
 
 const icons = [
-    'trash-01.png',
-    'trash-02.png',
-    'trash-03.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png',
-    'wave.png', 
-]
+    "trash-01.png",
+    "trash-02.png",
+    "trash-03.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+    "wave.png",
+];
 
-reloadBtn.addEventListener('click', () => {
-    location.reload()
-})
+reloadBtn.addEventListener("click", () => {
+    location.reload();
+});
 
 const addFlashMsg = (points) => {
-    flashMsg.innerText = `+${points}`
-    flashMsg.classList.remove('hide')
+    flashMsg.innerText = `+${points}`;
+    flashMsg.classList.remove("hide");
 
     setTimeout(() => {
-        flashMsg.classList.add('hide')
-    }, 500)
-}
+        flashMsg.classList.add("hide");
+    }, 500);
+};
 
 const revelCard = ({ target }) => {
     if (plays > 0) {
-        if (!target.parentNode.classList.contains('reveal-card')) {
-            const frontImg = target.parentNode.querySelector('.front').style.backgroundImage
-            target.parentNode.classList.add('reveal-card')
+        if (!target.parentNode.classList.contains("reveal-card")) {
+            const frontImg =
+                target.parentNode.querySelector(".front").style.backgroundImage;
+            target.parentNode.classList.add("reveal-card");
 
-            if (frontImg.includes('trash-01.png')) {
-                currentPoints += 50
-                addFlashMsg(50)
-            } else if (frontImg.includes('trash-02.png')) {
-                currentPoints += 100
-                addFlashMsg(100)
-            } else if (frontImg.includes('trash-03.png')) {
-                currentPoints += 150
-                addFlashMsg(150)
+            if (frontImg.includes("trash-01.png")) {
+                currentPoints += 50;
+                addFlashMsg(50);
+            } else if (frontImg.includes("trash-02.png")) {
+                currentPoints += 100;
+                addFlashMsg(100);
+            } else if (frontImg.includes("trash-03.png")) {
+                currentPoints += 150;
+                addFlashMsg(150);
             }
 
-            plays -= 1
+            plays -= 1;
         }
     } else {
-        reloadBtn.classList.remove('hide')
-        finalPointsMsg.innerText = `Parabéns! Você fez ${currentPoints} pontos`
-        finalPointsMsg.classList.remove('hide')
+        const allCards = document.querySelectorAll(".card");
+        allCards.forEach((card) => card.classList.add("reveal-card"));
+
+        reloadBtn.classList.remove("hide");
+        finalPointsMsg.innerText = `Parabéns! Você fez ${currentPoints} pontos`;
+        finalPointsMsg.classList.remove("hide");
     }
 
-    remaningPlays.innerText = plays
-    points.innerText = currentPoints.toString().padStart(2, '0');
-}
+    remaningPlays.innerText = plays;
+    points.innerText = currentPoints.toString().padStart(2, "0");
+};
 
 const newCard = (frontImg) => {
-    const card = document.createElement('div')
-    card.className = 'card'
+    const card = document.createElement("div");
+    card.className = "card";
 
-    const front = document.createElement('div')
-    front.className = 'face front'
-    front.style.backgroundImage = `url('./img/${frontImg}')`
+    const front = document.createElement("div");
+    front.className = "face front";
+    front.style.backgroundImage = `url('./img/${frontImg}')`;
 
-    const back = document.createElement('div')
-    back.className = 'face back'
+    const back = document.createElement("div");
+    back.className = "face back";
 
-    card.appendChild(front)
-    card.appendChild(back)
+    card.appendChild(front);
+    card.appendChild(back);
 
-    card.addEventListener('click', revelCard)
+    card.addEventListener("click", revelCard);
 
-    return card
-}
+    return card;
+};
 
 const loadGame = () => {
-    const shuffledIcons = [ ...icons, ...icons, ...icons, ...icons ].sort(() => Math.random() - 0.5)
+    const shuffledIcons = [...icons, ...icons, ...icons, ...icons].sort(
+        () => Math.random() - 0.5
+    );
 
     shuffledIcons.forEach((img) => {
-        const card = newCard(img)
-        cardsContainer.appendChild(card)
-    })
+        const card = newCard(img);
+        cardsContainer.appendChild(card);
+    });
+};
 
-}
-
-loadGame()
+loadGame();
